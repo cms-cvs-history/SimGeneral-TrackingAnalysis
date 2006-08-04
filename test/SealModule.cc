dc1a136ff5934c898f834f1ee10edfd2f30259bb
Eduardo Luiggi Lopez <sha1-3fc92667b1684b58ee3cf7c3d0fd548d6ae2812f@cern.ch>
2006-08-04 16:05:54 +0000

    Remove references to MCAccess

--- test/SealModule.cc
+++ test/SealModule.cc
@@ -1,6 +1,4 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "SimDataFormats/TrackingAnalysis/test/MCAccessTest.h"
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE( MCAccessTest );
